ca32cf5699a96708403ad8e17de3722a2342167d
Pablo Sepúlveda <psep.pentauc@gmail.com>
2023-05-16 22:12:47 -0400

    feat(service): LFD2 - cambio en el mensaje

---
+++ src/main/java/com/example/testingweb/Constantes.java
@@ -0,0 +1,9 @@
+package com.example.testingweb;
+
+public final class Constantes {
+
+    private Constantes(){}
+
+    public static final String MENSAJE = "Finaliza el curso DevOps Foundation v4 - Pablo Sepulveda (alumno)";
+    
+}
