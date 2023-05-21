6308df53f7138ca8c39d11f9658fce608d58c5f9
Pablo Sepúlveda <psep.pentauc@gmail.com>
2023-05-21 17:13:35 -0400

    test: add test code

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -1,17 +1,23 @@
 package com.example.testingweb;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-
-	private static final String PASSWORD = "test";
 	
 	public String greet() {
+		try {
+			Cipher.getInstance("DES");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return Constantes.MENSAJE;
 	}
 
-	public Boolean validatePassword(String password) {
-		return PASSWORD.equals(password);
-	}
 }
