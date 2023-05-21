1357f32492a5d21bbc27932defe07c3ab752fb50
Pablo Sepúlveda <psep.pentauc@gmail.com>
2023-05-21 17:04:41 -0400

    test: add test code

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -4,7 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+
+	private static final String PASSWORD = "test";
+	
 	public String greet() {
 		return Constantes.MENSAJE;
+	}
+
+	public Boolean validatePassword(String password) {
+		return PASSWORD.equals(password);
 	}
 }
